3e0d47b343a7cf98a0406ccfd3ef6a7f3a9ffc1d
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-21 22:48:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,16 @@
 class PigLatinizer
-  def piglatinize(word)
+  def piglatinize(sentence)
+    
+  end
+  
+  private
+  
+  def piglatinize_word(word)
     return word + 'way' if /^[aeiouAEIOU]/.match?(word)
     
     start = /^[^aeiou]*/.match(word).to_s
     
     return word.sub(/^[^aeiou]*/, '') + start + 'ay'
   end
+  
 end
